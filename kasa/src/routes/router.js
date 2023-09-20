5b8a86a265c70a35437660542ed1f46e5bdb3178
Teinturier Thibaut <thibaut.teinturier18@gmail.com>
2023-09-20 09:12:33 +0200

    Soutenance Ready

--- kasa/src/routes/router.js
+++ kasa/src/routes/router.js
@@ -2,7 +2,7 @@ import { createBrowserRouter, Navigate } from "react-router-dom"
 import Layout from "../components/Layout"
 import About from "../pages/About"
 import Homepage from "../pages/Homepage"
-import ErrorPage from "./route-error"
+import ErrorPage from "../pages/RouteError"
 import HousingDetails from "../pages/HousingDetails"
 
 const router = createBrowserRouter([
